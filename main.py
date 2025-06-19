15bfd2440608942cfed69744cf12d0343fd5b1b6
Nathan Kropiewnicki <142477799+NathanKropiewnicki@users.noreply.github.com>
2025-06-19 15:22:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -139,3 +139,9 @@ def messages():
     except Exception as e:
         print("Error:", e)
         return "", 200
+        
+if __name__ == "__main__":
+    init_db()
+    port = int(os.environ.get("PORT", 5000))  # Read Render's assigned port
+    app.run(host="0.0.0.0", port=port)        # Bind to 0.0.0.0
+
